7cda412b9f352a61e92a0f80c2a6dba03214d035
mibhur-mahajan <mhjnmibhur23@gmail.com>
2026-06-22 20:04:24 +0530

    Initial local setup

---
+++ src/conditionals_and_loops.java
@@ -0,0 +1,203 @@
+
+void main() {
+//    int salary = 25400;
+//    if(salary>10000){
+//        salary = salary + 2000;
+//    } else{
+//        salary = salary + 1000;
+//    }
+//    System.out.println(salary);
+
+    // multiple if else statements:
+
+//    int salary = 25400;
+//    if(salary > 10000){
+//        salary += 2000;
+//    } else if(salary>20000){
+//        salary += 3000;
+//    } else{
+//        salary += 1000;
+//    }
+//    System.out.println(salary);
+
+    // Q: Print numbers from 1 to 5:
+
+//    for(int num=1;num<=5;num+=1){
+//        System.out.println(num);
+//    }
+
+    // Q: Print numbers from 1 to n;
+
+//    Scanner input = new Scanner(System.in);
+//    int n = input.nextInt();
+//    for (int num = 1; num <= n; num++) {
+//        System.out.println(num + " ");
+//    }
+
+    // while loops:
+
+//    int num = 1;
+//    while(num<=5){
+//        System.out.println(num);
+//        num+=1;
+//    }
+
+//    Scanner in = new Scanner(System.in);
+//    int n = in.nextInt();
+//    int num = 1;
+//    while(num<=n){
+//        System.out.println("Hello World");
+//        num+=1;
+//    }
+
+    // do while loop:
+
+//    int n = 1;
+//    do{
+//        System.out.println("Hello World");
+//    } while(n!=1);
+
+//    int n = 1;
+//    do{
+//        System.out.println(n);
+//        n++;
+//    } while(n<=5);
+
+    // Ques 1: Largest of three numbers:
+
+//    Scanner in = new Scanner(System.in);
+//    int a = in.nextInt();
+//    int b = in.nextInt();
+//    int c = in.nextInt();
+//    int max = a;
+//    if(b>max){
+//        max = b;
+//    }
+//    if(c>max){
+//        max = c;
+//    }
+//    System.out.println(max);
+
+    // 2nd method:
+
+//    Scanner in = new Scanner(System.in);
+//    int a = in.nextInt();
+//    int b = in.nextInt();
+//    int c = in.nextInt();
+//    int max = 0;
+//    if(a>b){
+//        max = a;
+//    } else{
+//        max = b;
+//    }
+//    if(c>max){
+//        max = c;
+//    }
+//    System.out.println(max);
+
+    // 3rd Method:
+
+//    Scanner in = new Scanner(System.in);
+//    int a = in.nextInt();
+//    int b = in.nextInt();
+//    int c = in.nextInt();
+//    int max = Math.max(c,Math.max(a,b));
+//    System.out.println(max);
+
+      // Ques 2: Check string is uppercase or lowercase:
+
+//    Scanner in = new Scanner(System.in);
+//    char ch = in.next().trim().charAt(0); // converts input string to char
+//    if(ch>='a' && ch<='z'){
+//        System.out.println("Lowercase");
+//    } else{
+//        System.out.println("Uppercase");
+//    }
+
+    // Ques 3: Find the nth Fibonacci number:
+
+//    Scanner in = new Scanner(System.in);
+//    int n = in.nextInt();
+//    int a = 0;
+//    int b = 1;
+//    int count = 2; // to start with second iteration as first two numbers are given
+//    while(count<=n){
+//        int temp = b;
+//        b = b + a;
+//        a = temp; // we are not doing a=b and doing a=temp because temp stores the updated value of b
+//        count++;
+//    }
+//    System.out.println(b);
+
+    // Ques 4: To find the count of the repeating digits in a number:
+
+//    int n = 45535;
+//    int count = 0;
+//    while(n>0){
+//        int rem = n % 10;
+//        if(rem == 5){
+//            count++;
+//        }
+//        n = n / 10;
+//    }
+//    System.out.println(count);
+
+    // Ques 5: Find the reverse of a number:
+
+//    Scanner in = new Scanner(System.in);
+//    int num = in.nextInt();
+//    int ans = 0;
+//    while(num>0){
+//        int rem = num % 10;
+//        num /= 10;
+//        ans = ans * 10 + rem;
+//    }
+//    System.out.println(ans);
+
+    // Ques 6: CALCULATOR PROGRAM:
+
+    Scanner in = new Scanner(System.in);
+    // Take input from user till user does not press X or x
+    int ans = 0;
+    while(true){
+        // take the operator as input
+        System.out.println("Enter the operator: ");
+        char op = in.next().trim().charAt(0);
+        System.out.println(); // new line
+        if(op == '+' || op == '-' || op == '*' || op == '/' || op == '%'){
+            // input two numbers
+            System.out.println("Enter two numbers:");
+            int num1 = in.nextInt();
+            int num2 = in.nextInt();
+            System.out.println();
+            if(op == '+'){
+                ans = num1 + num2;
+            }
+            if(op == '-'){
+                ans = num1 - num2;
+            }
+            if(op == '*'){
+                ans = num1 * num2;
+            }
+            if(op == '/'){
+                if( num2 != 0){
+                    ans = num1 / num2;
+                }
+            }
+            if(op == '%'){
+                ans = num1 % num2;
+            }
+        } else if(op == 'x' || op == 'X'){
+            break;
+        } else {
+            System.out.println("Invalid Operation");
+        }
+        System.out.println(ans);
+    }
+
+
+
+
+
+
+}
---
+++ src/input_output.java
@@ -0,0 +1,97 @@
+
+
+void main() {
+//        System.out.println("HELLO WORLD");
+//        System.out.print("hello world");
+//        System.out.println("Hello World");
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println(input.nextInt());
+//        System.out.println(input.nextLine());
+//        System.out.println(input.next());
+
+          // Primitive Data types:
+//        int roll_no = 64;
+//        char letter = 'r';
+//        float marks = 98.67f;
+//        double largeDecimalNumbers = 34455453.2344;
+//        long largeInteger = 34355242343324L;
+//        boolean check = false;
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter your roll number: ");
+//        int roll_no = input.nextInt();
+//        System.out.println("Your roll number is " + roll_no);
+
+//          int a = 234_000_000; // it is 2crore 34lakh underscore_ will get ignored
+//          System.out.println(a);
+
+//          Scanner input = new Scanner(System.in);
+//          String name = input.next();
+//          System.out.println(name);
+//          float marks = input.nextFloat();
+//          System.out.println(marks);
+
+        // SUM OF TWO NUMBERS:
+//        Scanner input = new Scanner(System.in);
+//        int num1 = input.nextInt();
+//        int num2 = input.nextInt();
+//        int sum = num1 + num2;
+//        System.out.println("Sum = " + sum);
+
+        // TYPE CASTING AND TYPE CONVERSION:
+
+//        int num = (int)(67.56f);
+//        System.out.println(num);
+
+        // automatic type promotion in expressions
+//        int a = 257;
+//        byte b = (byte)(a); // 257%256 = 1, maximum a byte can store is 256
+//        System.out.println(b);
+
+//        byte a = 40;
+//        byte b = 50;
+//        byte c = 100;
+//        int d = (a*b) / c;
+//        System.out.println(d);
+
+//        int number = 'A';
+//        System.out.println(number); //print ASCII values
+
+//          byte b = 42;
+//          char c = 'a';
+//          short s = 1024;
+//          int i = 50000;
+//          float f = 5.67f;
+//          double d = 0.1234;
+//          double result = (f * b) + (i / c) - (d - s);
+//          // float + int - double = double
+//          System.out.println((f * b) + " " + (i / c) + " " + (d - s));
+//          System.out.println(result);
+
+
+          // WHILE LOOP
+
+//          int count = 1;
+//          while(count != 5){
+//                  System.out.println(count);
+//                  count++;
+//          }
+
+          // FOR LOOP
+
+//        for(int count = 1; count!= 5; count++){
+//                System.out.println(count);
+//        }
+
+        // CONVERT CELSIUS TO FAHRENHEIT:
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter temp in C: ");
+        float tempC = input.nextFloat();
+        float tempF = (tempC * 9/5) + 32;
+        System.out.println(tempF);
+}
+
+
+
