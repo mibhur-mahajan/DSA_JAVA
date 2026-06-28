c92f58a546bddbcbd8bac977291008651630230c
mibhur-mahajan <mhjnmibhur23@gmail.com>
2026-06-28 22:30:44 +0530

    arrays continued

--- src/Arrays_ArrayList.java
+++ src/Arrays_ArrayList.java
@@ -13,8 +13,63 @@ public class Arrays_ArrayList {
 //        int[] rnos2 = {23, 12, 45, 32, 15};
 //        System.out.println(rnos2.length);
 
-        int[] ros; // declaration of array. ros is getting defined in the stack
-        ros = new int[5]; // initialisation: actually here object is being created in the heap memory
-        System.out.println(ros[1]);
+//        int[] ros; // declaration of array. ros is getting defined in the stack
+//        ros = new int[5]; // initialisation: actually here object is being created in the heap memory
+//        System.out.println(ros[1]); // Prints 0
+//    }
+
+//        String[] arr = new String[4];
+//        System.out.println(arr[0]); // Prints null
+
+//        int [] arr = new int[5];
+//        arr[0] = 23;
+//        arr[1] = 45;
+//        arr[2] = 233;
+//        arr[3] = 543;
+//        arr[4] = 3;
+//        // [23, 45, 233, 543, 3]
+//        System.out.println(arr[3]);
+
+        // input using for loops:
+
+//        Scanner in = new Scanner(System.in);
+//        int[] arr = new int[5];
+//        for(int i = 0; i < arr.length; i++) {
+//            arr[i] = in.nextInt();
+//        }
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i] + " ");
+//        }
+
+        // Enhanced for loop:
+
+//        Scanner in = new Scanner(System.in);
+//        int[] arr = new int[5];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = in.nextInt();
+//        }
+//        for (int num : arr) { // for every element in array, print the element
+//            System.out.println(num + " "); // her num represents element of array
+//        }
+
+        // Best way of printing array:
+
+//        Scanner in = new Scanner(System.in);
+//        int[] arr = new int[5];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = in.nextInt();
+//        }
+//        System.out.println(Arrays.toString(arr));
+
+        // array of objects:
+
+        Scanner in = new Scanner(System.in);
+        String[] str = new String[4];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = in.next();
+        }
+        System.out.println(Arrays.toString(str));
+
+
     }
 }
