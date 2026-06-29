0341dc4d75d7e9562a59534f31fab6803dac24f7
mibhur-mahajan <mhjnmibhur23@gmail.com>
2026-06-29 23:46:36 +0530

    modified Arrays_ArrayList.java

--- src/Arrays_ArrayList.java
+++ src/Arrays_ArrayList.java
@@ -63,12 +63,14 @@ public class Arrays_ArrayList {
 
         // array of objects:
 
-        Scanner in = new Scanner(System.in);
-        String[] str = new String[4];
-        for (int i = 0; i < str.length; i++) {
-            str[i] = in.next();
-        }
-        System.out.println(Arrays.toString(str));
+//        Scanner in = new Scanner(System.in);
+//        String[] str = new String[4];
+//        for (int i = 0; i < str.length; i++) {
+//            str[i] = in.next();
+//        }
+//        System.out.println(Arrays.toString(str));
+
+        // Passing in Functions:
 
 
     }
