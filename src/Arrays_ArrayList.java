e63303ed18da7a2cce810baf432ce79e144b4b53
mibhur-mahajan <mhjnmibhur23@gmail.com>
2026-06-29 23:46:58 +0530

    Arrays continued

--- src/Arrays_ArrayList.java
+++ src/Arrays_ArrayList.java
@@ -72,6 +72,62 @@ public class Arrays_ArrayList {
 
         // Passing in Functions:
 
+//        int[] nums = {3, 4, 5, 12};
+//        System.out.println(Arrays.toString(nums));
+//        change(nums);
+//        System.out.println(Arrays.toString(nums));
+//
+//    }
+//    static void change(int[] arr) {
+//        arr[0] = 99;
+//    }
+
+        // Multidimensional/2-D Arrays:
+
+//        int[][] arr = new int[3][];
+//
+//        int[][] arr2D = {
+//                {1, 2, 3}, // 0th index
+//                {4, 5}, // 1st index
+//                {6, 7, 8, 9} // 2nd index -> arr2D[2] = {6, 7, 8, 9}
+//        };
+
+//          int[][] arr = new int[3][2];
+////          System.out.println(arr.length); // no of rows
+//        // input:
+//        Scanner in = new Scanner(System.in);
+//        for (int row = 0; row < arr.length; row++) {
+//            // for each col in every row
+//            for (int col = 0; col < arr[row].length; col++) {
+//                arr[row][col] = in.nextInt();
+//            }
+//        }
+//        // output:
+////        for (int row = 0; row < arr.length; row++) {
+////            // for each col in every row
+////            for (int col = 0; col < arr[row].length; col++) {
+////                System.out.println(arr[row][col] + " ");
+////            }
+////        }
+//
+//        // better way of output using enhanced for loop:
+//        for (int row = 0; row < arr.length; row++) {
+//            System.out.println(Arrays.toString(arr[row]));
+//        }
+
+        // Column not fixed:
+
+        int[][] arr = {
+                {1, 2, 3, 4},
+                {5, 6},
+                {7, 8, 9}
+        };
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
