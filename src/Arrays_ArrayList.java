0e5068d70e4ede470bad74dd4e9ef12da5489bf0
mibhur-mahajan <mhjnmibhur23@gmail.com>
2026-06-30 21:53:02 +0530

    Reverse array question

--- src/Arrays_ArrayList.java
+++ src/Arrays_ArrayList.java
@@ -231,4 +231,19 @@ public class Arrays_ArrayList {
         reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        while (start < end) {
+            // swap
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
 }
