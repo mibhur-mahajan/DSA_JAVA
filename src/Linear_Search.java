61b6d244d31e70650713b2530f2747e1ff12f623
mibhur-mahajan <mhjnmibhur23@gmail.com>
2026-07-03 23:47:58 +0530

    Maximum value in 2D array

--- src/Linear_Search.java
+++ src/Linear_Search.java
@@ -139,27 +139,27 @@ public class Linear_Search {
 
         // Ques 5: Maximum value in 2D array:
 
-//        int[][] arr = {
-//                {23, 4, 1},
-//                {18, 12, 3, 9},
-//                {78, 99, 34, 56},
-//                {18, 12}
-//        };
-//        int ans = max(arr);
-//        System.out.println((ans));
-//        System.out.println(Integer.MIN_VALUE);
-//    }
-//
-//    static int max(int[][] arr) {
-//        int max = Integer.MIN_VALUE;
-//        for (int row = 0; row < arr.length; row++) {
-//            for (int col = 0; col < arr[row].length; col++) {
-//                int element = arr[row][col];
-//                if (element > max) {
-//                    max = element;
-//                }
-//            }
-//        }
-//        return max;
-//    }
-//}
+        int[][] arr = {
+                {23, 4, 1},
+                {18, 12, 3, 9},
+                {78, 99, 34, 56},
+                {18, 12}
+        };
+        int ans = max(arr);
+        System.out.println((ans));
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+    static int max(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                int element = arr[row][col];
+                if (element > max) {
+                    max = element;
+                }
+            }
+        }
+        return max;
+    }
+}
