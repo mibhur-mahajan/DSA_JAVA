0c68a2aa6572b0a6d0b6b6c046e24078f779dc6b
mibhur-mahajan <mhjnmibhur23@gmail.com>
2026-07-13 22:10:48 +0530

    floor of a number

--- src/Binary_Search_Interview_Question.java
+++ src/Binary_Search_Interview_Question.java
@@ -38,30 +38,30 @@ public class Binary_Search_Interview_Question {
 
         // Floor of a Number:
 
-        int[] arr = {2, 3, 5, 9, 14, 16, 18};
-        int target = 15;
-        int ans = floor(arr, target);
-        System.out.println(ans);
-    }
-    // return the index
-    static int floor(int[] arr, int target) {
-        int start = 0;
-        int end = arr.length - 1;
-        while(start <= end) {
-            // find the middle element
-            // int mid = (start + end) / 2; // might be possible that that (start + end) exceeds the range of integer in java
-            int mid = start + (end - start) / 2;
-            if(target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                // ans found
-                return mid;
-            }
-        }
-        return end;
-    }
+//        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+//        int target = 15;
+//        int ans = floor(arr, target);
+//        System.out.println(ans);
+//    }
+//    // return the index
+//    static int floor(int[] arr, int target) {
+//        int start = 0;
+//        int end = arr.length - 1;
+//        while(start <= end) {
+//            // find the middle element
+//            // int mid = (start + end) / 2; // might be possible that that (start + end) exceeds the range of integer in java
+//            int mid = start + (end - start) / 2;
+//            if(target < arr[mid]) {
+//                end = mid - 1;
+//            } else if (target > arr[mid]) {
+//                start = mid + 1;
+//            } else {
+//                // ans found
+//                return mid;
+//            }
+//        }
+//        return end;
+//    }
 
     // Leetcode 744 :
     // Leetcode 34 :
