c6428d9da45e5f2326682a26222d64c62257545e
mibhur-mahajan <mhjnmibhur23@gmail.com>
2026-07-13 22:11:06 +0530

    position of element in infinite array

--- src/Binary_Search_Interview_Question.java
+++ src/Binary_Search_Interview_Question.java
@@ -65,4 +65,33 @@ public class Binary_Search_Interview_Question {
 
     // Leetcode 744 :
     // Leetcode 34 :
+
+    // Ques: Find position of an element in a sorted array of infinite numbers:
+
+
+        static int findingRange(int[] arr, int target) {
+            // first find the range
+            // first start with a box of size 2
+            int start = 0;
+            int end = 1;
+            // condition for the target to lie in the range
+            
+        }
+        static int infinite_position(int[] arr, int target, int start, int end)  {
+            while(start <= end) {
+                int mid = start + (end - start)/2;
+                if(target < arr[mid]) {
+                    end = mid - 1;
+                } else if(target > arr[mid]) {
+                    start = mid + 1;
+                } else {
+                    // ans found
+                    return mid;
+                }
+            }
+            return -1;
+        }
+
+        }
+
 }
