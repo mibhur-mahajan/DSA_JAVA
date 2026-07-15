41e9f3e361e4fc479cee6558ee0a2312482f243c
mibhur-mahajan <mhjnmibhur23@gmail.com>
2026-07-15 22:02:45 +0530

    Rotated Array Count

--- src/Binary_Search_Interview_Question.java
+++ src/Binary_Search_Interview_Question.java
@@ -279,3 +279,47 @@ public class Binary_Search_Interview_Question {
         int pivot = findPivot(arr);
         return pivot + 1;
     }
+      static int findPivot(int[] arr) {
+            int start = 0;
+            int end = arr.length - 1;
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                // Case 1: Mid is the pivot
+                if (mid < end && arr[mid] > arr[mid + 1]) {
+                    return mid;
+                }
+
+                // Case 2: Mid - 1 is the pivot
+                if (mid > start && arr[mid] < arr[mid - 1]) {
+                    return mid - 1;
+                }
+
+                // if elements at middle, start, end are equal then just skip the duplicates
+                if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                    // skip the duplicates
+                    // NOTE: what if these elements at start and end were the pivot?
+                    // check if start is pivot
+                    if (arr[start] > arr[start + 1]) {
+                        return start;
+                    }
+                    start++;
+                    // check whether end is pivot
+                    if (arr[end] < arr[end - 1]) {
+                        return end - 1;
+                    }
+                    end--;
+                }
+                // Search in the left half
+                if (arr[mid] <= arr[start]) {
+                    end = mid - 1;
+                } else {
+                    // Search in the right half
+                    start = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
